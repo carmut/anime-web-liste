940a41ec6a93dd38691b321c83d8519d85a45936
carmut <cloe.sirol@gmail.com>
2024-07-31 15:09:41 +0200

    revert upload

--- src/Controller/AjoutAnimeController.php
+++ src/Controller/AjoutAnimeController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Entity\Anime;
 use App\Form\AnimeAddType;
 use Doctrine\ORM\EntityManagerInterface;
-use GuzzleHttp\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -21,45 +20,7 @@ class AjoutAnimeController extends AbstractController
         $form->handleRequest($request);
            
         if ($form->isSubmitted() && $form->isValid()){
-
-            $imageUrl = $anime->getImageLink();
-
-            // Validation de l'URL de l'image
-            if (filter_var($imageUrl, FILTER_VALIDATE_URL)) {
-                $imageHeaders = @get_headers($imageUrl, 1);
-
-                if ($imageHeaders && strpos($imageHeaders[0], '200')) {
-                    $contentType = $imageHeaders['Content-Type'];
-                    $allowedTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/jpg'];
-                    if (in_array($contentType, $allowedTypes)) {
-                        $extension = explode('/', $contentType)[1];
-                        $imageName = uniqid() . '.' . $extension;
-                        $imagePath = $this->getParameter('kernel.project_dir') . '/public/images/' . $imageName;
-
-                        // Sauvegarder l'image sur le serveur
-                        $imageContent = @file_get_contents($imageUrl, false, stream_context_create([
-                            "http" => [
-                                "header" => "User-Agent:Mozilla/5.0"
-                            ]
-                        ]));
-                        if ($imageContent) {
-                            file_put_contents($imagePath, $imageContent);
-                            // Mettre à jour l'URL de l'image avec le chemin local
-                            $anime->setImageLink('/images/' . $imageName);
-
-                        } else {
-                            $this->addFlash('error', 'Impossible de télécharger l\'image.');
-                        }
-                    } else {
-                        $this->addFlash('error', 'Le type de fichier n\'est pas supporté.');
-                    }
-                } else {
-                    $this->addFlash('error', 'URL invalide ou non supportée.');
-                }
-            } else {
-                $this->addFlash('error', 'URL invalide ou non supportée.');
-            }
-
+            
             $entityManager->persist($anime);
             $entityManager->flush();
 
